00324f73ad49fd350e085e691bbd5ca5588498ae
suvorovjr <pglavan1998@mail.ru>
2024-05-07 00:11:25 +0300

    add ConfirmEmailAPIView in users views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 from .models import User
+from .services import validate_token
 
 
 class UserCreateAPIView(generics.CreateAPIView):
@@ -11,8 +14,12 @@ class UserCreateAPIView(generics.CreateAPIView):
 
 
 class ConfirmEmailAPIView(APIView):
-    def get(self):
-        pass
+    def get(self, request, token, *args, **kwargs):
+        if validate_token(token):
+            return Response({'message': 'Email успешно подтвержден.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Ссылка для активации недействительна или устарела.'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
