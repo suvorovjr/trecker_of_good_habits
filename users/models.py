339f5b2ccf59d3ea2913c1e8382e396a11cf7d83
suvorovjr <pglavan1998@mail.ru>
2024-05-07 00:10:06 +0300

    add changes in User model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,9 @@ NULLABLE = {'null': True, 'blank': True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='E-mail')
-    avatar = models.ImageField(upload_to='/avatars', verbose_name='Аватар', **NULLABLE)
-    telegram_id = models.CharField(max_length=35, unique=True, verbose_name='ID Телеграм')
+    avatar = models.ImageField(upload_to='avatars', verbose_name='Аватар', **NULLABLE)
+    telegram_id = models.CharField(max_length=35, verbose_name='ID Телеграм', **NULLABLE)
+    is_active = models.BooleanField(default=False, verbose_name='Активация')
+    email_confirmation_token = models.CharField(max_length=255, verbose_name='Токен', **NULLABLE)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
