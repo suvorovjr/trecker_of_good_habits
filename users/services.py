32750d8204e03bea6a15395f15df660bab4a1802
suvorovjr <pglavan1998@mail.ru>
2024-05-07 01:34:44 +0300

    add validate token for users in services.py

--- users/services.py
+++ users/services.py
@@ -1,8 +1,10 @@
 import jwt
+import pytz
 from datetime import timedelta
 from django.core.mail import send_mail
 from django.utils import timezone
 from django.conf import settings
+from .models import User
 
 
 def get_confirmation_token(user):
@@ -15,7 +17,6 @@ def get_confirmation_token(user):
     }
 
     token = jwt.encode(payload=token_payload, key=settings.SECRET_KEY, algorithm='HS256')
-    # decode_token = jwt.decode(jwt=token, key=settings.SECRET_KEY, algorithms='HS256')
     return token
 
 
@@ -24,5 +25,24 @@ def send_confirm_link(recipient_list, confirm_link):
         subject='Подтвердите аккаунт',
         message=f'Для подтверждения аккаунта перейдите по ссылке {confirm_link}',
         from_email=settings.EMAIL_HOST_USER,
-        recipient_list=recipient_list
+        recipient_list=recipient_list.split()
     )
+
+
+def validate_token(token):
+    try:
+        decode_token = jwt.decode(jwt=token, key=settings.SECRET_KEY, algorithms='HS256')
+        user_id = decode_token.get('id')
+        user = User.objects.get(pk=user_id, is_active=False)
+        if user.email_confirmation_token != token:
+            return False
+        current_time = timezone.now()
+        moscow_tz = pytz.timezone('Europe/Moscow')
+        expiration_date = timezone.datetime.fromtimestamp(decode_token.get('exp'), tz=moscow_tz)
+        if expiration_date < current_time:
+            return False
+        user.is_active = True
+        user.save()
+    except (jwt.ExpiredSignatureError, jwt.DecodeError, User.DoesNotExist):
+        return False
+    return True
