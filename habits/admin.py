dce2906707d3207d88a3ab23723d4e3cdd7f4a41
suvorovjr <pglavan1998@mail.ru>
2024-05-07 21:30:03 +0300

    add Habit validators

--- habits/admin.py
+++ habits/admin.py
@@ -4,4 +4,4 @@ from .models import Habit
 
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
-    list_display = ('habit_place', 'habit_time', 'regularity', 'execution_time', 'is_publish', 'is_pleasant')
+    list_display = ('habit_place', 'habit_time', 'pk', 'regularity', 'execution_time', 'is_publish', 'is_pleasant')
