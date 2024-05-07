c58c6d4d0416756d06db5a885ee894370d92f3de
suvorovjr <pglavan1998@mail.ru>
2024-05-07 21:36:31 +0300

    add Habit validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Habit
+from .validators import IsPleasantValidator, RelatedHabit
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+        validators = [IsPleasantValidator(field='is_pleasant'), RelatedHabit(field='related_habit')]
+
+
+
---
+++ habits/validators.py
@@ -0,0 +1,24 @@
+from rest_framework.validators import ValidationError
+from .models import Habit
+
+
+class IsPleasantValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        if value.get(self.field):
+            if value.get('award') or value.get('related_habit'):
+                raise ValidationError('У приятной привычки не может быть вознаграждения или связанной привычки.')
+
+
+class RelatedHabit:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        related_habit = value.get(self.field)
+        if related_habit and not related_habit.is_pleasant:
+            raise ValidationError('В связанные привычки могут попадать только привычки с признаком приятной привычки.')
+        elif related_habit and value.get('award'):
+            raise ValidationError('Не может быть заполнено и поле вознаграждения, и поле связанной привычки.')
